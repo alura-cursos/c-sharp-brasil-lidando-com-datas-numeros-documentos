3ba08e55109af63dbc646b49ad8b4cced72b9441
marcelooliveira <mclricardo@gmail.com>
2017-06-29 17:00:14 -0300

    Aula4
    Aula4

---
+++ CEP/Program.cs
@@ -0,0 +1,42 @@
+﻿using Caelum.Stella.CSharp.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cep = "01001000";
+            string result = GetEndereco(cep);
+            Debug.WriteLine(result);
+
+            ViaCEP viaCEP = new ViaCEP();
+            string enderecoJson = viaCEP.GetEnderecoJson(cep);
+            Debug.WriteLine(enderecoJson);
+
+            string enderecoXml = viaCEP.GetEnderecoXml(cep);
+            Debug.WriteLine(enderecoXml);
+
+            var task = viaCEP.GetEnderecoJsonAsync(cep);
+            Debug.WriteLine(task.Result);
+
+            var endereco = viaCEP.GetEndereco(cep);
+            Debug.WriteLine(string.Format("Logradouro: {0}, Bairro: {1}", endereco.Logradouro, endereco.Bairro));
+        }
+
+        private static string GetEndereco(string cep)
+        {
+            string url = "https://viacep.com.br/ws/" + cep + "/json/";
+
+            string result = new HttpClient().GetStringAsync(url).Result;
+            return result;
+        }
+    }
+}
