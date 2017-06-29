660c2f277373d47f49cd3ce57baf92809b53be8a
marcelooliveira <mclricardo@gmail.com>
2017-06-29 16:54:29 -0300

    Aula2
    Aula2

---
+++ NumerosPorExtenso/Program.cs
@@ -0,0 +1,33 @@
+﻿using Caelum.Stella.CSharp.Inwords;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosPorExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = 75;
+            string extenso = new Numero(valor).Extenso();
+            Debug.WriteLine(extenso);
+
+            valor = 1532987;
+            extenso = new Numero(valor).Extenso();
+            Debug.WriteLine(extenso);
+
+            Debug.WriteLine(extenso + " Reais");
+
+            string extensoBRL = new MoedaBRL(valor).Extenso();
+            Debug.WriteLine(extensoBRL);
+
+            valor = 1532987.89;
+            extensoBRL = new MoedaBRL(valor).Extenso();
+            Debug.WriteLine(extensoBRL);
+        }
+    }
+}
---
+++ TrabalhandoComDatas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhandoComDatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime data = new DateTime(2017, 9, 23);
+            Debug.WriteLine(data);
+            Debug.WriteLine(data.ToString("d"));
+            Debug.WriteLine(data.ToString("d", new CultureInfo("pt-BR")));
+            Debug.WriteLine(data.ToString("dd/MM"));
+            Debug.WriteLine(data.ToString("dd/MM/yy"));
+
+            data = new DateTime(2017, 9, 23, 13, 14, 15, 987);
+            Debug.WriteLine(data);
+            Debug.WriteLine(data.ToString("HH:mm"));
+            Debug.WriteLine(data.ToString("HH:mm:ss.fff"));
+
+            Debug.WriteLine(data.ToString("D"));
+            Debug.WriteLine(data.ToString("m"));
+            Debug.WriteLine(data.ToString("Y"));
+
+            Debug.WriteLine(data.ToString("G"));
+            Debug.WriteLine(data.ToString("g"));
+
+            Debug.WriteLine(data.ToString("O"));
+            Debug.WriteLine(DateTime.Parse(data.ToString("O")).ToString("dd/MM/yyyy HH:mm:ss.fff"));
+
+            Debug.WriteLine(data.ToString("t"));
+            Debug.WriteLine(data.ToString("T"));
+
+        }
+    }
+}
